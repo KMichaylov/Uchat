24955c69d4321c6df47ee1e6e9277db92df9f5d5
Kristiyan Michaylov <krisidm24@gmail.com>
2024-09-15 00:11:39 +0200

    Implement further tailwind configuration for react

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      "primary-background-color": "#BAE8E8",
+      "primary-button-color": "#487BFF",
+      "danger-color": "#D90429",
+      "warning-color": "#FFD803",
+    },
+    backgroundImage: {
+      "background-pattern": "url('/frontend/src/assets/background.png')",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
-
+};
