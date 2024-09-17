ec670a03b0723931cb92ec08a26a1c5110fb391a
Kristiyan Michaylov <krisidm24@gmail.com>
2024-09-17 23:28:06 +0200

    Fix typo

--- frontend/src/models/InputModel.ts
+++ frontend/src/models/InputModel.ts
@@ -1,13 +1,11 @@
 import { ChangeEventHandler } from "react";
-
-
 export interface InputModel {
     inputLabel: string;
     inputValue: string;
     inputPlaceholder?: string;
-    type: "emial" | "image" | "password" | "text";
+    type: "email" | "image" | "password" | "text";
     labelName: string;
-    onChange: ChangeEventHandler<HTMLInputElement> | undefined;
-    stylingInput: string;
-    stylingLabel: string;
+    onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
+    stylingInput?: string;
+    stylingLabel?: string;
 }
