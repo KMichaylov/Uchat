b538747add0c2e12961a181c626e06082b91bf4c
Kristiyan Michaylov <krisidm24@gmail.com>
2024-11-13 22:45:00 +0100

    Adapt formatting of Registration page

--- frontend/src/pages/Registration.tsx
+++ frontend/src/pages/Registration.tsx
@@ -1,5 +1,14 @@
 import { CustomForm } from "../components/Form/CustomForm";
 import { RegistrationFormModel } from "../models/RegistrationModel";
+import { Logo } from "../components/Logo/Logo";
+
+const handleSubmit = async (isValid, formValues) => {
+  if (isValid) {
+    console.log("Form is valid", formValues);
+  } else {
+    console.log("Form is not valid");
+  }
+};
 
 const registrationConfig: RegistrationFormModel = {
   type: "REGISTRATION",
@@ -11,24 +20,21 @@ const registrationConfig: RegistrationFormModel = {
   onSubmit: handleSubmit,
 };
 
-const handleFormSubmit = async (isValid, formValues) => {
-  if (isValid) {
-    console.log("Form is valid");
-  } else {
-    console.log("Form is not valid");
-  }
-};
-
 function RegistrationPage() {
   return (
     <>
-      <div className="bg-background-pattern">
-        <div className="container mx-auto bg-white rounded-lg shadow-lg p-8 max-w-lg">
-          {/* TODO: Create the logo component */}
-          <Logo />
-          <CustomForm {...registrationConfig} />
+      <div className="min-h-screen bg-background-pattern bg-cover bg-center flex items-center justify-center px-4">
+        <div className="container mx-auto bg-white rounded-lg shadow-lg p-8 max-w-lg w-full">
+          <div className="flex flex-col space-y-8">
+            <div className="flex justify-center gap-y-10">
+              <Logo />
+            </div>
+            <CustomForm {...registrationConfig} />
+          </div>
         </div>
       </div>
     </>
   );
 }
+
+export default RegistrationPage;
