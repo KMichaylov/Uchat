00c53ab71a182b01a4b8a87de7a1564b33a34844
Kristiyan Michaylov <krisidm24@gmail.com>
2024-11-13 22:10:36 +0100

    Add routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RegistrationPage from "./pages/Registration.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/registration", element: <RegistrationPage /> },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
